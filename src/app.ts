80207175b1532536ef92dac2efc1f93ffd2fa41b
Andrés Zapata Mazo <andrez.mazo@gmail.com>
2024-11-02 01:46:28 -0500

    - Se añade cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import expenseRoutes from "./routes/expenseRoutes";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/api/expenses", expenseRoutes);
